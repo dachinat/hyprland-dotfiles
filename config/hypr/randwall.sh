ed5eaf31648f63367606e58197a81e2127c50f29
Dachi Natsvlishvili <dachinat@gmail.com>
2025-12-09 01:05:07 +0400

    Edit wallpaper path

--- config/hypr/randwall.sh
+++ config/hypr/randwall.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-swww img `find ~/Pictures/Wallpapers-Hyprland -type f | shuf -n 1` --transition-type simple --transition-fps 60
-
+swww img `find ~/wallpapers -type f | shuf -n 1` --transition-type simple --transition-fps 60
